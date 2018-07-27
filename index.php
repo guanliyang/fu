5c69733960674dbe71db729da643a59f0b42fcb4
guanliyang <1145828184@qq.com>
2018-07-27 14:18:47 +0800

    adfd

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
-<?php
-echo "hello workd";
+# fu项目
+1, 安装了nginx，配置文件目录  nginx -h 查看
+
+vim /etc/nginx/conf.d/default.conf
+
+
+
+2, 项目代码目录  /data/fu xia
+
+
+
+3，安装了php   php -v  5.3.3
+
+
+
+4, 安装了mysql   用户名 root  密码
