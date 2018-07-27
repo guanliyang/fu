a00ded235a5967f0597cf2f821041b21f2a7b9cf
liyang <you@example.com>
2018-07-27 15:58:09 +0800

    asfdf

---
+++ Application/Home/Model/UserModel.class.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: guanliyang
+ * Date: 2018/7/27
+ * Time: 下午3:13
+ */
+namespace Home\Model;
+use Think\Model;
+class UserModel extends Model {
+    protected $tableName = 'user';
+}
