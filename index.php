31095d82b17a703b976d1a333460954beb245ac5
liyang <you@example.com>
2018-07-27 14:38:11 +0800

    添加thinkphp3.2

--- index.php
+++ index.php
@@ -1,16 +1,26 @@
-# fu项目
-1, 安装了nginx，配置文件目录  nginx -h 查看
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2014 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
 
-vim /etc/nginx/conf.d/default.conf
+// 应用入口文件
 
+// 检测PHP环境
+if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
+// 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
+define('APP_DEBUG',True);
 
-2, 项目代码目录  /data/fu xia
+// 定义应用目录
+define('APP_PATH','./Application/');
 
+// 引入ThinkPHP入口文件
+require './ThinkPHP/ThinkPHP.php';
 
-
-3，安装了php   php -v  5.3.3
-
-
-
-4, 安装了mysql   用户名 root  密码
+// 亲^_^ 后面不需要任何代码了 就是如此简单
