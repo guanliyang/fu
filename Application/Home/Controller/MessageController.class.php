fa8a9134b5611c5ad1614d44e64e262c693d224f
liyang <you@example.com>
2018-07-28 14:57:53 +0800

    adsf

--- Application/Home/Controller/MessageController.class.php
+++ Application/Home/Controller/MessageController.class.php
@@ -4,6 +4,6 @@ namespace Home\Controller;
 class MessageController extends HomeController {
     public function index() {
         $this->checkoutLogin();
-        echo "我登录了";
+        echo "i'm in ";
     }
 }
