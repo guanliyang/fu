4e8d20e855162cd8b089fc6f15f4fd13c710d30a
liyang <you@example.com>
2018-07-30 14:41:39 +0800

    asfdd

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -3,7 +3,7 @@ function is_mobile($mobile) {
     return preg_match("/^1\d{10}$/", $mobile);
 }
 
-function notice($message, $code = 1, $data = []) {
+function notice($message, $code = 1, $data = array()) {
     header('Content-type: application/json;charset=utf-8');
     die(json_encode(array('code' => $code, 'msg' => $message, 'ts' => time()) + $data));
 }
