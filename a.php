4cb282234136975953d490cdcfcb033eb6e012db
liyang <you@example.com>
2018-08-03 18:13:45 +0800

    asdfdsf

--- a.php
+++ a.php
@@ -2,4 +2,5 @@
 file_put_contents('a.log', json_encode($_POST), FILE_APPEND);
 file_put_contents('a.log', json_encode($_FILES), FILE_APPEND);
 
-move_uploaded_file($_FILES["image"]["tmp_name"], "/Public/user_img" . $_FILES["image"]["name"]);
+$a = move_uploaded_file($_FILES["image"]["tmp_name"], "Public/user_img" . $_FILES["image"]["name"]);
+file_put_contents('a.log', json_encode($a), FILE_APPEND);
