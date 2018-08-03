535a3bedceabc9d9e79dbc76ef85bd1a7a78f43b
liyang <you@example.com>
2018-08-03 18:10:40 +0800

    sdfdsf

--- a.php
+++ a.php
@@ -1,3 +1,5 @@
 <?php
 file_put_contents('a.log', json_encode($_POST), FILE_APPEND);
 file_put_contents('a.log', json_encode($_FILES), FILE_APPEND);
+
+move_uploaded_file($_FILES["image"]["tmp_name"], "/Public/user_img" . $_FILES["image"]["name"]);
