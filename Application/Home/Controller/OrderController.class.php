bca48b742810e96e53dedee2ab23d27c0a936ba3
liyang <you@example.com>
2018-08-03 11:14:16 +0800

    生成订单

---
+++ Application/Home/Controller/OrderController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+
+class OrderController extends HomeController {
+    // 进入订单结算
+    public function add() {
+        $order = new \Home\Model\OrderModel();
+        $order->addOrder();
+        die("生成订单");
+    }
+
+}
---
+++ Application/Home/Model/OrderModel.class.php
@@ -0,0 +1,9 @@
+<?php
+namespace Home\Model;
+use Think\Model;
+class OrderModel extends Model {
+    protected $trueTableName = 'b_order';
+
+    public function addOrder() {
+    }
+}
