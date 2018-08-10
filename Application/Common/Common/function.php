11ea3d92e36358060f24bc79bd1f9b54f5c5d088
liyang <you@example.com>
2018-08-10 17:44:19 +0800

    asfd

--- Application/Common/Common/function.php
+++ Application/Common/Common/function.php
@@ -67,23 +67,25 @@ function getOfferStatus($status) {
     return $list[$status];
 }
 
-function array_column(array $array, $column_key, $index_key=null){
-    $result = array();
-    foreach($array as $arr) {
-        if(!is_array($arr)) continue;
+if (!function_exists('array_column')) {
+    function array_column(array $array, $column_key, $index_key=null){
+        $result = array();
+        foreach($array as $arr) {
+            if(!is_array($arr)) continue;
 
-        if(is_null($column_key)){
-            $value = $arr;
-        }else{
-            $value = $arr[$column_key];
-        }
+            if(is_null($column_key)){
+                $value = $arr;
+            }else{
+                $value = $arr[$column_key];
+            }
 
-        if(!is_null($index_key)){
-            $key = $arr[$index_key];
-            $result[$key] = $value;
-        }else{
-            $result[] = $value;
+            if(!is_null($index_key)){
+                $key = $arr[$index_key];
+                $result[$key] = $value;
+            }else{
+                $result[] = $value;
+            }
         }
+        return $result;
     }
-    return $result;
 }
