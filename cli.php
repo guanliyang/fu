276e9231247f713fce43050cb3fcb5f145fded3c
liyang <you@example.com>
2018-09-16 00:49:22 +0800

    adsf

--- cli.php
+++ cli.php
@@ -16,11 +16,11 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
+$path = str_replace('cli.php', '', __FILE__);
 
 // 定义应用目录
-define('APP_PATH','./Application/');
+define('APP_PATH', $path.'/Application/');
 
-$path = str_replace('cli.php', '', __FILE__);
 
 // 引入ThinkPHP入口文件
 require $path.'/ThinkPHP/ThinkPHP.php';
