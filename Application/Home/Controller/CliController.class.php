26b6a264bca2808cb20243f6a8054d0feda48cdd
liyang <you@example.com>
2018-09-16 02:44:01 +0800

    adsf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -3,8 +3,7 @@ namespace Home\Controller;
 use Think\Controller;
 class CliController extends Controller{
     public function test1() {
-        $path = str_replace('cli.php', '', __FILE__);
         $msg = date('Y-m-d H:i:s')."\n";
-        file_put_contents($path.'/test/a.log', $msg, FILE_APPEND);
+        file_put_contents(C('PATH').'/test/a.log', $msg, FILE_APPEND);
     }
 }
--- cli.php
+++ cli.php
@@ -17,7 +17,7 @@ if(version_compare(PHP_VERSION,'5.3.0','<'))  die('require PHP > 5.3.0 !');
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG',True);
 $path = str_replace('cli.php', '', __FILE__);
-
+define('PATH', $path);
 // 定义应用目录
 define('APP_PATH', $path.'/Application/');
 
