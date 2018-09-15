12255eb2256524ac529af5e41c8eb8c11d66e8c7
liyang <you@example.com>
2018-09-16 02:47:56 +0800

    adsf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -4,6 +4,6 @@ use Think\Controller;
 class CliController extends Controller{
     public function test1() {
         $msg = date('Y-m-d H:i:s')."\n";
-        file_put_contents(C('PATH').'/test/a.log', $msg, FILE_APPEND);
+        file_put_contents(PATH.'/test/a.log', $msg, FILE_APPEND);
     }
 }
