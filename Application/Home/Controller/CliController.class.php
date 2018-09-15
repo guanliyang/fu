d081b8e210c1e226c1334d0af28684e28e716e12
liyang <you@example.com>
2018-09-16 02:32:04 +0800

    asdf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -3,7 +3,8 @@ namespace Home\Controller;
 use Think\Controller;
 class CliController extends Controller{
     public function test1() {
+        $path = str_replace('cli.php', '', __FILE__);
         $msg = date('Y-m-d H:i:s')."\n";
-        file_put_contents('./test/a.log', $msg, FILE_APPEND);
+        file_put_contents($path.'/test/a.log', $msg, FILE_APPEND);
     }
 }
