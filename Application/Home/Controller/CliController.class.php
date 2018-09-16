91e75805186f8643e2499262e2d15b8dbb032843
liyang <you@example.com>
2018-09-16 12:22:27 +0800

    asdf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -5,5 +5,6 @@ class CliController extends Controller{
     public function test1() {
         $msg = date('Y-m-d H:i:s')."\n";
         file_put_contents(PATH.'/test/a.log', $msg, FILE_APPEND);
+        M('sms_log')->data(array('message' => $msg))->add();
     }
 }
