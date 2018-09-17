aa1dbc4246295df7796c72eaddd544c6020f7013
liyang <you@example.com>
2018-09-17 09:55:16 +0800

    asdf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -5,6 +5,18 @@ class CliController extends Controller{
     public function test1() {
         $msg = date('Y-m-d H:i:s')."\n";
         file_put_contents(PATH.'/test/a.log', $msg, FILE_APPEND);
-//        M('sms_log')->data(array('message' => $msg))->add();
+    }
+
+    public function updateOrder() {
+        $order_list = M('b_order')->select();
+        if ($order_list && is_array($order_list)) {
+            foreach ($order_list as $order) {
+                if (!empty($order['o_artime']) &&
+                    $order['o_artime'] < time() &&
+                    $order['o_status'] == 7 ){
+                    M('b_order')->where(array('o_id' => $order['o_id']))->save(array('o_status' => 9));
+                }
+            }
+        }
     }
 }
