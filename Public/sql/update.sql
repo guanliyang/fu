c7c9c5dfa4bf43cdc6e6b29de34adaa9df244448
liyang <you@example.com>
2018-09-29 16:31:40 +0800

    asdf

--- Public/sql/update.sql
+++ Public/sql/update.sql
@@ -1,6 +1,7 @@
-update area set level=-1 where id in
+update local_area set level=-1 where id in
 (
-(select id from (SELECT id from area where level=1) a)
+(select id from (SELECT id from local_area where level=1) a)
 );
-update area set level=1 where id in (464,579);
-update area set parentId=-2 where id in (591,621,628,634,640);
+update local_area set level=1 where id in (464,579);
+update local_area set parentId=-2 where id in (591,621,628,634,640);
+
