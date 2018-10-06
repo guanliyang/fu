5a80a7c0dc096fea7bfe6f1722a7024a42daeb79
liyang <you@example.com>
2018-10-06 19:27:43 +0800

    saf

--- Application/Home/Controller/CliController.class.php
+++ Application/Home/Controller/CliController.class.php
@@ -7,16 +7,46 @@ class CliController extends Controller{
         file_put_contents(PATH.'/test/a.log', $msg, FILE_APPEND);
     }
 
-    public function updateOrder() {
-        $order_list = M('b_order')->where('o_artime IS NOT NULL AND o_artime < '.time().' AND o_status = 7')->select();
-        if ($order_list && is_array($order_list)) {
+//    public function updateOrder() {
+//        $order_list = M('b_order')->where('o_artime IS NOT NULL AND o_artime < '.time().' AND o_status = 7')->select();
+//        if ($order_list && is_array($order_list)) {
+//            foreach ($order_list as $order) {
+//                if (!empty($order['o_artime']) &&
+//                    $order['o_artime'] < time() &&
+//                    $order['o_status'] == 7 ){
+//                    M('b_order')->where(array('o_id' => $order['o_id']))->save(array('o_status' => 9));
+//                }
+//            }
+//        }
+//    }
+
+    public function updateOrder()
+    {
+        // (订单货组 状态为1，同时最晚收货时间不为空，同时小于当前时间），状态更新为2
+        $where_item = 'oi_status=1 AND oi_artime IS NOT NULL AND oi_artime < '.time();
+        $order_item_list = M('b_order_item')->where($where_item)->select();
+
+        if (!empty($order_item_list) && is_array($order_item_list)) {
+            foreach ($order_item_list as $item) {
+                M('b_order_item')->where(array('oi_id' => $item['oi_id']))->save(array('oi_status' => 2));
+            }
+        }
+
+        //表b_order 条件：o_status=7 AND 订单对应货组，全部货组状态oi_status=2时，更新订单状态为9
+        $where_order = 'o_status=7';
+        $order_list = M('b_order')->where($where_order)->select();
+
+        if (!empty($order_list) && is_array($order_list)) {
             foreach ($order_list as $order) {
-                if (!empty($order['o_artime']) &&
-                    $order['o_artime'] < time() &&
-                    $order['o_status'] == 7 ){
+                $all_count = M('b_order_item')->where('o_id='.$order['o_id'])->count();
+                $status_count = M('b_order_item')->where('o_id='.$order['o_id'].' AND oi_status=2')->count();
+
+                // 等于2的数量相同，证明都等于2
+                if ($all_count && $status_count && $all_count == $status_count) {
                     M('b_order')->where(array('o_id' => $order['o_id']))->save(array('o_status' => 9));
                 }
             }
         }
+
     }
 }
--- Public/sql/update.sql
+++ Public/sql/update.sql
@@ -27,3 +27,8 @@ insert into g_level set gl_name='二等',gl_id=2;
 
 -- 地区呕吐
 alter table `local_area` add COLUMN `val_ot` int(11) DEFAULT NULL;
+
+-- 自动脚本
+alter table `b_order_item` add column   `oi_status` int(11) unsigned DEFAULT '0' COMMENT '对应货组状态，1已卸货 2已收货';
+
+alter table `b_order_item` add column  `oi_artime` int(11) unsigned DEFAULT NULL COMMENT '最晚确认收货时间（物流运营点击确认到货/确认已自提时，更新此数据字段当前时间+全局变量确认收货时间）/客户端 用点击击“确认收货”按钮时，更新此数据字段为点击时时间/系统自动脚本运行，根据此数据荐更新状态为2 已收货';
